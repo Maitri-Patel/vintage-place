a99d338a27347250a93f04e76628a36867d44ec1
Maitri-Patel <mpatel18@academic.rrc.ca>
2024-03-31 18:44:44 -0500

    databse setup is done

---
+++ db/migrate/20240331214146_create_order_tracks.rb
@@ -0,0 +1,12 @@
+class CreateOrderTracks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :order_tracks do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity
+      t.decimal :price_at_purchase
+
+      t.timestamps
+    end
+  end
+end
